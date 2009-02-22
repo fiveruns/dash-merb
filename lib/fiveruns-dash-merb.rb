78909848f1bf74c7cb84c4598029b73195be88e2
Bruce Williams <bruce@codefluency.com>
2009-02-22 03:16:44 -0600

    Support for 'autoadding' ORM recipes, warning if not found * Should automatically load/configure recipes from fiveruns-dash-activerecord and fiveruns-dash-datamapper (& fiveruns-dash-sequel, when available) Bump version to 0.8.0 due to small change in custom recipe configuration (now [name, options] vs [name, url])

--- lib/fiveruns-dash-merb.rb
+++ lib/fiveruns-dash-merb.rb
@@ -1,18 +1,21 @@
 # Depends on non-ActiveSupport fiveruns-dash-ruby (>= 0.8.0)
 dependency 'fiveruns-dash-ruby', '>= 0.8.0', :require_as => 'fiveruns/dash',
                                              :immediate => true
+
 Merb::Config[:dash] ||= {
   :token => nil,
+  :autoadd_orm => true,
   :recipes   => [
     # This recipe can be removed, if desired
-    [:merb, 'http://dash.fiveruns.com']
+    [:merb, {:url => 'http://dash.fiveruns.com'}]
   ]
 }
 
 Merb::BootLoader.after_app_loads do
-  Fiveruns::Dash.logger = Merb.logger
   require 'fiveruns/dash/merb'
   if Merb::Config[:adapter] != 'irb' && Merb::Config[:dash][:token]
+    Fiveruns::Dash.logger = Merb.logger
+    Fiveruns::Dash::Merb.autoadd_orm
     Fiveruns::Dash::Merb.start
   end
 end
--- lib/fiveruns/dash/merb.rb
+++ lib/fiveruns/dash/merb.rb
@@ -7,11 +7,26 @@ module Fiveruns::Dash
       ::Fiveruns::Dash.logger.info "Starting FiveRuns Dash"
       Fiveruns::Dash.start(:app => ::Merb::Config[:dash][:token]) do |config|
         Array(::Merb::Config[:dash][:recipes]).each do |set|
-          name, url = Array(set)
+          name, options = Array(set)
           ::Fiveruns::Dash.logger.debug "Adding FiveRuns Dash recipe: #{set.inspect}"
-          config.add_recipe(name, :url => url)
+          config.add_recipe(name, options)
         end
       end
+    end
+    
+    def self.autoadd_orm
+      return if !::Merb.orm || !::Merb::Config[:dash][:autoadd_orm]
+      require "fiveruns-dash-#{::Merb.orm}"
+      ::Merb::Config[:dash][:recipes].push([
+        ::Merb.orm,
+        {
+          :url => 'http://dash.fiveruns.com',
+          :total_time => 'response_time'
+        }
+      ])
+      Fiveruns::Dash.logger.info "Auto-added recipe for ORM from fiveruns-dash-#{::Merb.orm}"
+    rescue LoadError
+      Fiveruns::Dash.logger.warn "Could not auto-add recipe for ORM from fiveruns-dash-#{::Merb.orm}"
     end
       
   end
